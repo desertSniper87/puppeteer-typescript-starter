6678aaf9bfb567de8f226ce7f2a8794ac8431c9c
desertsniper87 <torshobuet@gmail.com>
2022-11-26 20:27:25 +0600

    minor update

--- src/pageScraper.ts
+++ src/pageScraper.ts
@@ -1,7 +1,8 @@
+import {Browser} from "puppeteer";
+
 const scraperObject = {
-  //Change url to link you're scraping from
   url: 'https://www.example.com',
-  async scraper(browser: any){
+  async scraper(browser: Browser){
       let page = await browser.newPage();
       console.log(`Navigating to ${this.url}...`);
       await page.goto(this.url);
